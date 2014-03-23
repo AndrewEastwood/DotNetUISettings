36e4f9b08f6934cca91a3a7d3c7dcbe588b5a92c
AndrewEastwood <ua.clients.support@gmail.com>
2014-03-23 14:40:02 +0200

    init

---
+++ components/Shared/Defaults/DefaultPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using components.Shared.Interfaces;
+using System.Collections;
+
+namespace components.Shared.Defaults
+{
+    public class DefaultPlugin// : IPlugin
+    {
+        private DefaultPluginAssembly assemblyInfo;
+
+        public DefaultPlugin(Hashtable pluginAssemblyInfo)
+        {
+            assemblyInfo = new DefaultPluginAssembly(pluginAssemblyInfo);
+        }
+        public DefaultPlugin()
+        {
+            assemblyInfo = new DefaultPluginAssembly();
+        }
+
+        public DefaultPluginAssembly Assembly
+        {
+            get { return assemblyInfo; }
+        }
+    }
+}
---
+++ testApplication/testApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using components.Public;
+
+namespace testApplication
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Instance.LoadConfigurationData();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new wndSettings());
+        }
+    }
+}
